17ee9aa727f77c11e7694707589d3dc85d913dc1
saori watanabe <saori.watanabe@asahinet.com>
2018-08-06 09:35:18 +0100

    index modified

--- serviceworker.js
+++ serviceworker.js
@@ -4,6 +4,7 @@ var cacheName = 'manekineko';
 //キャッシュしたいファイルのリストを登録する
 var filesToCache = [
    'index.html',
+   'main.js',
    'jquery.js',
    'style.css',
 ];
@@ -35,6 +36,9 @@ self.addEventListener('activate',function(event){
 
 //
 self.addEventListener('fetch',function(event){
+  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+    return;
+    }
     event.respondWith(
      caches.match(event.request).then(function(response){
      return response || fetch(event.request);
@@ -44,4 +48,21 @@ self.addEventListener('fetch',function(event){
 });
 
 
+self.addEventListener('push',function(event){
+   console.log('Push Notification Recieved',event);
+   if(Notification.permission=='granted'){
+   event.waitUntil(
+   self.registration.showNotification('受信しました').then(function(showEvent){//プッシュ通知に表示するメッセージ
+    }, function(error){
+      cosole.log(error);
+    })
+    );
+   }
+});
+
+self.addEventListener('notificationclick',function(event){
+    console.log('通知がクリックされました.',event.notification.tag);
+     event.notification.close();
+
+});
 
